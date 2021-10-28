21684db705d70080255a9ea95fd22efeb50e7f6e
Mathieu Dupré <mathieu.dupre@savoirfairelinux.com>
2021-10-28 16:55:39 +0200

    pxe: refactor pxe and dhcp_vm containers
    Merge pxe and dhcp_vm containers together. The new container can work as
    DHCP server for the VM or as a PXE server.
    
    With the separation of Jenkins pipelines, the container build step is done
    in the Jenkins_build pipelines which does not have access to Ansible
    inventory information. So we can't customize the Jenkins container with
    docker buildargs anymore. The customization must now be done with
    environment variables during the docker run. To facilitate this we add a
    script that will take care of this step.
    
    The PXE server can work with BIOS and UEFI.
    
    Issue: #6633
    Signed-off-by: Mathieu Dupré <mathieu.dupre@savoirfairelinux.com>
    Change-Id: Icf9039e1cac9691f94764c6dcdec9836f63fc056

---
+++ pxe/start.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+set -e
+
+rm -rf /tmp/dnsmasq.d
+mkdir /tmp/dnsmasq.d
+if [ -n "${PXE}" ] ; then
+    if [ "${PXE}" = uefi ] ; then
+        pxelinux=syslinux/efi64/syslinux.efi
+    else
+        pxelinux=syslinux/pxelinux.0
+    fi
+cat << EOF > /tmp/dnsmasq.d/pxe.conf
+enable-tftp=${BIND_INTERFACE}
+dhcp-option=66,${SERVER_ADDRESS}
+dhcp-option=67,${pxelinux}
+tftp-root=/tftpboot
+EOF
+sed  "s/@SERVER_ADDRESS@/${SERVER_ADDRESS}/g" /opt/syslinux.cfg.in >/tftpboot/syslinux/pxelinux.cfg/default
+sed  "s/@HTTP_PORT@/${HTTP_PORT}/g" -i /tftpboot/syslinux/pxelinux.cfg/default
+sed  "s/@SERVER_ADDRESS@/${SERVER_ADDRESS}/g" /opt/syslinux_efi.cfg.in > /tftpboot/syslinux/efi64/pxelinux.cfg/default
+sed  "s/@HTTP_PORT@/${HTTP_PORT}/g" -i /tftpboot/syslinux/efi64/pxelinux.cfg/default
+
+fi
+if [ "${DHCP_NO_IGNORE}" != "yes" ] ; then
+    echo "dhcp-ignore=tag:!known" > /tmp/dnsmasq.d/dhcp_ignore.conf
+fi
+
+cat << EOF > /tmp/dnsmasq.d/dhcp.conf
+dhcp-authoritative
+dhcp-range=${DHCP_RANGE_BEGIN},${DHCP_RANGE_END},48h
+interface=${BIND_INTERFACE}
+log-dhcp
+EOF
+
+exec /usr/sbin/dnsmasq --keep-in-foreground --log-facility=-
