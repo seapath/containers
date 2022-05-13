b6a53ecf1bfe286aa05601c68111df502e3ae534
Mathieu Dupré <mathieu.dupre@savoirfairelinux.com>
2022-05-13 21:55:30 +0200

    pxe: add support for dual BIOS / UEFI PXE
    The DHCP server detects the architecture and send the correct syslinux
    file.
    
    Issue: #7733
    Signed-off-by: Mathieu Dupré <mathieu.dupre@savoirfairelinux.com>
    Change-Id: I9894b235cccae5ea9821da647f79dd5bea51710f

--- pxe/start.sh
+++ pxe/start.sh
@@ -4,24 +4,42 @@ set -e
 
 rm -rf /tmp/dnsmasq.d
 mkdir /tmp/dnsmasq.d
+
 if [ -n "${PXE}" ] ; then
-    if [ "${PXE}" = uefi ] ; then
-        pxelinux=syslinux/efi64/syslinux.efi
+    if [ "${PXE}" = dual ] ; then
+        cat << EOF > /tmp/dnsmasq.d/pxe.conf
+enable-tftp=${BIND_INTERFACE}
+tftp-root=/tftpboot
+dhcp-match=set:x86PC, option:client-arch, 0 #BIOS
+dhcp-match=set:UEFI64, option:client-arch, 7 #UEFI64
+dhcp-match=set:UEFI64, option:client-arch, 9 #EBC
+
+dhcp-boot=tag:x86PC, syslinux/pxelinux.0
+dhcp-boot=tag:UEFI64, syslinux/efi64/syslinux.efi
+EOF
     else
-        pxelinux=syslinux/pxelinux.0
-    fi
-cat << EOF > /tmp/dnsmasq.d/pxe.conf
+        if [ "${PXE}" = uefi ] ; then
+            pxelinux=syslinux/efi64/syslinux.efi
+        else
+            pxelinux=syslinux/pxelinux.0
+        fi
+        cat << EOF > /tmp/dnsmasq.d/pxe.conf
 enable-tftp=${BIND_INTERFACE}
 dhcp-option=66,${SERVER_ADDRESS}
 dhcp-option=67,${pxelinux}
 tftp-root=/tftpboot
 EOF
-sed  "s/@SERVER_ADDRESS@/${SERVER_ADDRESS}/g" /opt/syslinux.cfg.in >/tftpboot/syslinux/pxelinux.cfg/default
-sed  "s/@HTTP_PORT@/${HTTP_PORT}/g" -i /tftpboot/syslinux/pxelinux.cfg/default
-sed  "s/@SERVER_ADDRESS@/${SERVER_ADDRESS}/g" /opt/syslinux_efi.cfg.in > /tftpboot/syslinux/efi64/pxelinux.cfg/default
-sed  "s/@HTTP_PORT@/${HTTP_PORT}/g" -i /tftpboot/syslinux/efi64/pxelinux.cfg/default
-
+    fi
+    sed  "s/@SERVER_ADDRESS@/${SERVER_ADDRESS}/g" \
+        /opt/syslinux.cfg.in >/tftpboot/syslinux/pxelinux.cfg/default
+    sed  "s/@HTTP_PORT@/${HTTP_PORT}/g" \
+        -i /tftpboot/syslinux/pxelinux.cfg/default
+    sed  "s/@SERVER_ADDRESS@/${SERVER_ADDRESS}/g" \
+        /opt/syslinux_efi.cfg.in > /tftpboot/syslinux/efi64/pxelinux.cfg/default
+    sed  "s/@HTTP_PORT@/${HTTP_PORT}/g" \
+        -i /tftpboot/syslinux/efi64/pxelinux.cfg/default
 fi
+
 if [ "${DHCP_NO_IGNORE}" != "yes" ] ; then
     echo "dhcp-ignore=tag:!known" > /tmp/dnsmasq.d/dhcp_ignore.conf
 fi
